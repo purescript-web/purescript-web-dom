309634a18f7574de9bbf398ba0a339ee16de5cb1
Gary Burgess <gary.burgess@gmail.com>
2018-05-14 17:48:35 +0100

    Add missing text coercions

--- src/Web/DOM/Text.purs
+++ src/Web/DOM/Text.purs
@@ -3,11 +3,23 @@ module Web.DOM.Text where
 import Effect (Effect)
 import Unsafe.Coerce (unsafeCoerce)
 import Web.DOM.CharacterData (CharacterData)
+import Web.DOM.ChildNode (ChildNode)
+import Web.DOM.Internal.Types (Node)
+import Web.DOM.NonDocumentTypeChildNode (NonDocumentTypeChildNode)
 
 foreign import data Text :: Type
 
 toCharacterData :: Text -> CharacterData
 toCharacterData = unsafeCoerce
+
+toChildNode :: Text -> ChildNode
+toChildNode = unsafeCoerce
+
+toNonDocumentTypeChildNode :: Text -> NonDocumentTypeChildNode
+toNonDocumentTypeChildNode = unsafeCoerce
+
+toNode :: Text -> Node
+toNode = unsafeCoerce
 
 foreign import splitText :: Int -> Text -> Effect Text
 
