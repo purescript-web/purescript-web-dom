73c34bf0412871703a1f6b21da765b3d0b010679
Serhii Khoma <srghma@gmail.com>
2020-05-21 08:45:32 +0300

    refactor: NodeType -> remove spaces

--- src/Web/DOM/NodeType.purs
+++ src/Web/DOM/NodeType.purs
@@ -37,18 +37,18 @@ instance boundedEnumNodeType :: BoundedEnum NodeType where
   fromEnum = fromEnumNodeType
  
 instance showNodeType :: Show NodeType where
-  show ElementNode               = "ElementNode"
-  show AttributeNode             = "AttributeNode"
-  show TextNode                  = "TextNode"
-  show CDATASectionNode          = "CDATASectionNode"
-  show EntityReferenceNode       = "EntityReferenceNode"
-  show EntityNode                = "EntityNode"
+  show ElementNode = "ElementNode"
+  show AttributeNode = "AttributeNode"
+  show TextNode = "TextNode"
+  show CDATASectionNode = "CDATASectionNode"
+  show EntityReferenceNode = "EntityReferenceNode"
+  show EntityNode = "EntityNode"
   show ProcessingInstructionNode = "ProcessingInstructionNode"
-  show CommentNode               = "CommentNode"
-  show DocumentNode              = "DocumentNode"
-  show DocumentTypeNode          = "DocumentTypeNode"
-  show DocumentFragmentNode      = "DocumentFragmentNode"
-  show NotationNode              = "NotationNode"
+  show CommentNode = "CommentNode"
+  show DocumentNode = "DocumentNode"
+  show DocumentTypeNode = "DocumentTypeNode"
+  show DocumentFragmentNode = "DocumentFragmentNode"
+  show NotationNode = "NotationNode"
 
 toEnumNodeType :: Int -> Maybe NodeType
 toEnumNodeType 1 = Just ElementNode
