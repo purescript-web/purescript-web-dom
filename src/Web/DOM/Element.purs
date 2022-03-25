09508a028a9fe4da61c25573c48060159414c65a
JordanMartinez <jordanalex.martinez@protonmail.com>
2022-03-25 09:35:55 -0500

    Port getBoundingClientRect from web-html (#53)
    * Port getBoundingClientRect from web-html
    
    * Add changelog entry

--- src/Web/DOM/Element.purs
+++ src/Web/DOM/Element.purs
@@ -38,6 +38,8 @@ module Web.DOM.Element
   , clientLeft
   , clientWidth
   , clientHeight
+  , getBoundingClientRect
+  , DOMRect
   , ShadowRootInit
   , attachShadow
   ) where
@@ -143,6 +145,19 @@ foreign import clientTop :: Element -> Effect Number
 foreign import clientLeft :: Element -> Effect Number
 foreign import clientWidth :: Element -> Effect Number
 foreign import clientHeight :: Element -> Effect Number
+
+type DOMRect =
+  { top :: Number
+  , right :: Number
+  , bottom :: Number
+  , left :: Number
+  , width :: Number
+  , height :: Number
+  , x :: Number
+  , y :: Number
+  }
+
+foreign import getBoundingClientRect :: Element -> Effect DOMRect
 
 type ShadowRootInit = {
   mode :: ShadowRootMode,
